6ae0be9a0fe71f2be1fcfa874a0eb8fce25a155d
Ahmed Madhoun <68607257+ahmed-madhoun1@users.noreply.github.com>
2024-12-17 18:49:07 +0200

    Update build.gradle.kts

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,109 +1,88 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     kotlin("multiplatform")
+    kotlin("plugin.serialization") // For KMP serialization if needed
     id("com.android.library")
-    id("org.jetbrains.compose")
-    id("org.jetbrains.dokka")
-    id("com.vanniktech.maven.publish")
+    id("maven-publish") // For publishing to GitHub
 }
 
 kotlin {
-    androidTarget {
-        publishLibraryVariants("release")
-    }
-
-    jvm("desktop")
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64(),
-    ).forEach { iosTarget ->
-        iosTarget.binaries.framework {
-            baseName = "lib"
-            isStatic = true
-        }
-    }
+    android()
+    iosX64() // iOS simulator target
+    iosArm64() // iOS device target
+    // Add other targets as necessary (such as js, macosX64, etc.)
 
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material)
+                implementation("androidx.compose.ui:ui:1.5.0") // Use the appropriate Compose version
+                implementation("androidx.compose.material:material:1.5.0") // Material Compose
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2") // Optional Coroutines dependency
             }
         }
         val androidMain by getting {
             dependencies {
+                implementation("androidx.compose.ui:ui-tooling-preview:1.5.0") // For Android-specific tooling
             }
         }
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
-        val desktopMain by getting {
+        val iosMain by getting {
             dependencies {
+                // Add iOS-specific libraries or dependencies here if needed
             }
         }
     }
 }
 
 android {
-    compileSdk = (findProperty("android.compileSdk") as String).toInt()
-    namespace = "com.myapplication.common"
-
-    sourceSets["main"].manifest.srcFile("src/androidMain/AndroidManifest.xml")
-    sourceSets["main"].res.srcDirs("src/androidMain/res")
-    sourceSets["main"].resources.srcDirs("src/commonMain/resources")
-
+    compileSdk = 34
     defaultConfig {
-        minSdk = (findProperty("android.minSdk") as String).toInt()
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlin {
-        jvmToolchain(17)
+        minSdk = 21
+        targetSdk = 34
     }
 }
 
-mavenPublishing {
-//    publishToMavenCentral(SonatypeHost.DEFAULT)
-    // or when publishing to https://s01.oss.sonatype.org
-    publishToMavenCentral(SonatypeHost.S01, automaticRelease = true)
-    signAllPublications()
-    coordinates("com.am.nativeprogressindicatorcmp", "nativeprogressindicatorcmp-runtime", "1.0.0")
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["android"])
 
-    pom {
-        name.set(project.name)
-        description.set("A description of what my library does.")
-        inceptionYear.set("2024")
-        url.set("https://github.com/ahmed-madhoun1/NativeProgressIndicatorCMP/")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            groupId = "com.am.nativeprogressindicatorcmp" // Updated package name
+            artifactId = "NativeProgressIndicatorCMP"
+            version = "1.0.0" // Version of your library
+
+            pom {
+                name.set("NativeProgressIndicatorCMP")
+                description.set("A Compose Multiplatform library for native progress indicators")
+                url.set("https://github.com/ahmed-madhoun1/NativeProgressIndicatorCMP")
+
+                scm {
+                    url.set("https://github.com/ahmed-madhoun1/NativeProgressIndicatorCMP.git")
+                    connection.set("scm:git:git://github.com/ahmed-madhoun1/NativeProgressIndicatorCMP.git")
+                    developerConnection.set("scm:git:git@github.com:ahmed-madhoun1/NativeProgressIndicatorCMP.git")
+                }
+                licenses {
+                    license {
+                        name.set("Apache 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ahmed-madhoun1")
+                        name.set("Ahmed Madhoun")
+                        email.set("your-email@example.com") // Update this with your email address
+                    }
+                }
             }
         }
-        developers {
-            developer {
-                id.set("ahmed-madhoun1")
-                name.set("Ahmed Madhoun")
-                url.set("https://github.com/ahmed-madhoun1/")
+    }
+
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/ahmed-madhoun1/NativeProgressIndicatorCMP")
+            credentials {
+                username = project.findProperty("gpr.user") ?: System.getenv("USERNAME_GITHUB")
+                password = project.findProperty("gpr.token") ?: System.getenv("TOKEN_GITHUB")
             }
-        }
-        scm {
-            url.set("https://github.com/ahmed-madhoun1/NativeProgressIndicatorCMP/")
-            connection.set("scm:git:git://github.com/ahmed-madhoun1/NativeProgressIndicatorCMP.git")
-            developerConnection.set("scm:git:ssh://git@github.com/ahmed-madhoun1/NativeProgressIndicatorCMP.git")
         }
     }
 }
