92d22da508ca706955fa6a1f6abf27323a0eb1e5
Ahmed Madhoun <68607257+ahmed-madhoun1@users.noreply.github.com>
2024-12-27 17:58:21 +0200

    Update NativeProgressIndicator.kt

--- lib/src/commonMain/kotlin/com.am.nativeprogressindicatorcmp/NativeProgressIndicator.kt
+++ lib/src/commonMain/kotlin/com.am.nativeprogressindicatorcmp/NativeProgressIndicator.kt
@@ -2,8 +2,6 @@ package com.am.nativeprogressindicatorcmp
 
 import androidx.compose.runtime.Composable
 
-expect fun NativeProgressIndicator()
-
 @Composable
 fun NativeProgressIndicatorComponent() {
     NativeProgressIndicator()
